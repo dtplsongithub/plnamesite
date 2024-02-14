e8f6d5fa5b68b43485f6e2b5a51e74cbac5128dd
dtplsongithub <153951662+dtplsongithub@users.noreply.github.com>
2024-02-14 09:12:37 +0000

    super maybe probably fixed #3

--- script.js
+++ script.js
@@ -16,9 +16,8 @@ if (web[web.length - 3] == "archived") { // means its in a folder thats in /arch
   folder = false;
   web = web.splice(web.length-2, 1);
 }
-web = "/"+web.join("/")+"/";
 var webb = document.createElement("span");
-webb.innerText = web;
+webb.innerText = "/"+web.join("/")+"/";
 if (folder != null) document.body.appendChild(webb);
 if (folder) {
   var table = document.createElement("table");
@@ -26,20 +25,16 @@ if (folder) {
   row.insertCell(0).innerText = "Version name";
   row.insertCell(1).innerText = "Size";
   for (i in Object.keys(files[folder])) {
-    if (typeof Object.keys(files[folder])[i] == "number") {
       let row = table.insertRow(-1);
       row.insertCell(0).innerHTML = `<a href="${Object.keys(files[folder])[i]}.zip">${Object.keys(files[folder])[i]}.zip</a>`;
       row.insertCell(1).innerText = (files[folder][Object.keys(files[folder])[i]]/1024/1024).toFixed(2) + " MB";
-    } else {
-      let row = table.insertRow(-1);
-      row.insertCell(0).innerHTML = `<a href="./${Object.keys(files)[i]}/">${Object.keys(files)[i]}</a>`;
-    }
   }
   document.body.appendChild(table);
 } else if (folder != null) {
   var table = document.createElement("table");
   for (i in Object.keys(files)) {
-    
+    let row = table.insertRow(-1);
+    row.insertCell(0).innerHTML = `<a href="./${Object.keys(files)[i]}/">${Object.keys(files)[i]}</a>`;
   }
   document.body.appendChild(table);
 }
